5bed9ba0a57f02d4821b634b44aef7c396df20e9
Pablo Diego <pablodiegoss@hotmail.com>
2020-04-01 21:48:56 -0300

    Add marker creation with re-sizable border through -b

--- pymarker/cli.py
+++ pymarker/cli.py
@@ -5,15 +5,15 @@ from core import generate_png, generate_patt
 @click.argument('filename')
 @click.option('--patt','-p', is_flag=True, default=False)
 @click.option('--marker','-m', is_flag=True, default=False)
-def generate_patt_and_png(filename, patt, marker):
+@click.option('--border-size', '-b', default=84) # 84 is based on template hiro marker
+def generate_patt_and_png(filename, patt, marker, border_size):
     click.echo("-- Starting PyMarker Generator --".format(filename))
-
     if (patt and marker) or (not patt and not marker):
         generate_patt(filename)
-        generate_png(filename)
+        generate_png(filename, border_size)
         click.echo("Generating patt and marker for {}".format(filename))
     elif marker:
-        generate_png(filename)
+        generate_png(filename, border_size)
         click.echo("Generating marker for {}".format(filename))
     elif patt:
         generate_patt(filename)
--- pymarker/core.py
+++ pymarker/core.py
@@ -1,7 +1,20 @@
+from utils import open_image, get_box_coords, remove_extension
+from PIL import Image
+
+def get_marker_size(image, border_size):
+    # There are two borders (left and right) and (up and down)
+    size = image.height + (border_size*2)
+    # Using the height double because the resulting marker should be a square.
+    return (size,size)
 
 def generate_patt(filename):
     return "000"
 
-
-def generate_png(filename):
-    return "111"
+def generate_png(filename, border_size):
+    image = open_image(filename)
+    # Default color is black, setting (0, 0, 0) for clarity, as the border should be black
+    marker_size = get_marker_size(image, border_size)
+    marker = Image.new('RGB', marker_size, (0, 0, 0))
+    marker.paste(image,get_box_coords(image,border_size))
+    marker.save(remove_extension(filename)+".png", "PNG")
+    return True
