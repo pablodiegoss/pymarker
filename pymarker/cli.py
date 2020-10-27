8fdb13e7e820575a87f5966720683e7dee587e2b
Pablo Diego <pablodiegoss@hotmail.com>
2020-10-27 16:47:31 -0300

    Apply Black as default code formatting

--- pymarker/cli.py
+++ pymarker/cli.py
@@ -2,21 +2,22 @@ import click
 from .core import generate_marker, generate_patt
 from .utils import echo
 
+
 @click.command()
-@click.argument('filename')
-@click.option('--patt','-p', is_flag=True, default=False)
-@click.option('--marker','-m', is_flag=True, default=False)
-@click.option('--string','-s', is_flag=True, default=False)
-@click.option('--border-size', '-b', default=50) # 50% is based on template hiro marker
-@click.option('--output','-o', default=None, type=str)
+@click.argument("filename")
+@click.option("--patt", "-p", is_flag=True, default=False)
+@click.option("--marker", "-m", is_flag=True, default=False)
+@click.option("--string", "-s", is_flag=True, default=False)
+@click.option("--border-size", "-b", default=50)  # 50% is based on template hiro marker
+@click.option("--output", "-o", default=None, type=str)
 def generate_patt_and_marker(filename, patt, marker, string, border_size, output):
     echo("-- Starting PyMarker Generator --".format(filename), silent=string)
     if (patt and marker) or (not patt and not marker):
-        echo("Generating patt and marker for {}".format(filename),silent=string)
+        echo("Generating patt and marker for {}".format(filename), silent=string)
         generate_patt(filename, output, string)
         generate_marker(filename, border_size, output)
     elif marker:
-        echo("Generating marker for {}".format(filename),silent=string)
+        echo("Generating marker for {}".format(filename), silent=string)
         generate_marker(filename, border_size, output)
     elif patt:
         echo("Generating patt for {}".format(filename), silent=string)
@@ -24,8 +25,10 @@ def generate_patt_and_marker(filename, patt, marker, string, border_size, output
 
     echo("Done.", silent=string)
 
+
 def main():
     generate_patt_and_marker()
+
 
 if __name__ == "__main__":
     main()
--- pymarker/utils.py
+++ pymarker/utils.py
@@ -1,62 +1,71 @@
-import os 
+import os
 from PIL import Image
 import click
 
+
 def get_current_dir():
     # Slash added at the end to append with filenames
-    return os.path.abspath('.') + '/'
+    return os.path.abspath(".") + "/"
+
 
 def open_image(filename):
-    image_path = get_current_dir() + filename    
+    image_path = get_current_dir() + filename
     try:
         image = Image.open(image_path)
     except IOError:
         raise FileNotFoundError
-    
+
     return square_image(image)
 
+
 def square_image(image):
-    limited_size = image.width if image.height>image.width else image.height
-    return image.resize((limited_size,limited_size))
-    
+    limited_size = image.width if image.height > image.width else image.height
+    return image.resize((limited_size, limited_size))
+
+
 # Coords used when pasting the original image inside the black canvas to create borders
 def get_box_coords(image, border_size):
     # left, upper, right, lower bounds in pixels
-    return (border_size,
-            border_size, 
-            border_size + image.width,
-            border_size + image.height
-            )
+    return (
+        border_size,
+        border_size,
+        border_size + image.width,
+        border_size + image.height,
+    )
 
 
 # The filename contains extension and/or folders that should be filtered
 def remove_extension(filename):
     return filename.split(".")[0]
 
+
 # Get folder location of file
 def get_dir(filename):
     # Avoid error on paths without '/' at the end
-    folder = filename if filename[-1] == "/" else filename+"/"
-    return folder.rsplit("/", 2)[0]+"/"
+    folder = filename if filename[-1] == "/" else filename + "/"
+    return folder.rsplit("/", 2)[0] + "/"
+
 
 # Get name of the file from a filepath
 def get_name(filename):
     name = filename.rsplit("/", 1)[1]
     return name.split(".")[0]
 
+
 # Check and return folder path with '/' if necessary
 def check_path(path):
-    path = path if path[-1] == "/" else path+"/"
+    path = path if path[-1] == "/" else path + "/"
     return path
+
 
 def echo(string, silent=False, *params, **kwargs):
     if not silent:
         click.echo(string, *params, **kwargs)
 
-class PattStr:
 
+class PattStr:
     def __init__(self):
-       self.content = ''
+        self.content = ""
 
     def __repr__(self):
         return self.content.rstrip()
