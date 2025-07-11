616ae55e878c93883e2a2a721b37b1d432be6cb9
Pablo Silva <pablodiegoss@hotmail.com>
2025-07-11 21:55:51 +0100

    Refactor marker tests to include white borders

--- tests/png_tests.py
+++ tests/png_tests.py
@@ -1,33 +1,25 @@
 import unittest
 from pymarker import generate_marker
+from pymarker.exceptions import BlackBorderSizeError, WhiteBorderSizeError
 import filecmp as fc
 import os
 
-KEEP_FILES = True  # Set to True to keep generated files after tests
+KEEP_FILES = False  # Set to True to keep generated files after tests
 class TestMarkerGenerator(unittest.TestCase):
     def test_output_marker(self):
-        """Testing that marker is being generated"""
+        """Test that marker is generated and matches the expected output."""
         input_image = "tests/input/hiro.jpg"
+        output_file = "tests/input/hiro_marker.png"
         generate_marker(input_image)
-        f = open("tests/input/hiro_marker.png", "r")
-        if f:
-            try:
-                assert fc.cmp(
-                    "tests/input/hiro_marker.png", "tests/output/hiro_marker_b50.png"
-                )
-            except:
-                assert False
-            finally:
-                f.close()
-                if not KEEP_FILES:
-                    os.remove("tests/input/hiro_marker.png")
-            assert True
-        else:
-            assert False
+        try:
+            self.assertTrue(fc.cmp(output_file, "tests/output/hiro_marker_b20_w3.png"))
+        finally:
+            if os.path.exists(output_file) and not KEEP_FILES:
+                os.remove(output_file)
 
     def test_no_input(self):
         """Testing that no input raises an error for generating markers"""
-        border_size = 50
+        border_size = 20
         try:
             generate_marker(None, border_size, None)
         except FileNotFoundError:
@@ -38,9 +30,8 @@ class TestMarkerGenerator(unittest.TestCase):
         which defines an output folder for his marker"""
 
         input_image = "tests/input/hiro.jpg"
-        border_size = 50
         output_folder = "tests/automated/"
-        generate_marker(input_image, border_size, output_folder)
+        generate_marker(filename=input_image, output=output_folder)
         f = open(output_folder + "hiro_marker.png", "r")
         if f:
             f.close()
@@ -50,21 +41,70 @@ class TestMarkerGenerator(unittest.TestCase):
         else:
             assert False
 
-    def test_border_size(self):
+    def test_black_border_size(self):
         """Tests if the user can define a border size using -b
-        checking for border_size 50% which is default and 25%.
+        checking for border_size 20% which is default and 10%.
         """
         input_image = "tests/input/hiro.jpg"
-        border_size = 25
+        border_size = 10
         output_folder = "tests/automated/"
 
         generate_marker(input_image, border_size, output_folder)
         assert fc.cmp(
-            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b25.png"
+            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b10_w3.png"
         )
+        if not KEEP_FILES:
+            os.remove(output_folder + "hiro_marker.png")
 
-        border_size = 50
+        border_size = 20
         generate_marker(input_image, border_size, output_folder)
         assert fc.cmp(
-            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b50.png"
+            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b20_w3.png"
         )
+        if not KEEP_FILES:
+                os.remove(output_folder + "hiro_marker.png")
+    
+    def test_white_border_size(self):
+        """Tests if the user can define a white border size using -w
+        checking for border_size 3% which is default and 0% as not having a white border.
+        """
+        input_image = "tests/input/hiro.jpg"
+        border_size = 0
+        output_folder = "tests/automated/"
+
+        generate_marker(input_image, 20, output_folder, border_size)
+        assert fc.cmp(
+            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b20_w0.png"
+        )
+        if not KEEP_FILES:
+                os.remove(output_folder + "hiro_marker.png")
+
+        border_size = 20
+        generate_marker(input_image, 20, output_folder, border_size)
+        assert fc.cmp(
+            output_folder + "hiro_marker.png", "tests/output/hiro_marker_b20_w20.png"
+        )
+        if not KEEP_FILES:
+                os.remove(output_folder + "hiro_marker.png")
+
+
+    def test_invalid_border_sizes(self):
+        """Tests if the user can define a border size using -b
+        checking for border_size 0% and 50% which are invalid.
+        """
+        input_image = "tests/input/hiro.jpg"
+        output_folder = "tests/automated/"
+
+        # Testing black border size
+        with self.assertRaises(BlackBorderSizeError):
+            generate_marker(input_image, 0, output_folder)
+
+        with self.assertRaises(BlackBorderSizeError):
+            generate_marker(input_image, 50, output_folder)
+
+        # Testing white border size
+        with self.assertRaises(WhiteBorderSizeError):
+            generate_marker(input_image, 20, output_folder, -1)
+
+        with self.assertRaises(WhiteBorderSizeError):
+            generate_marker(input_image, 20, output_folder, 50)
